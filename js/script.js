d0c7ff6db463162954edb885766eee46b791bf09
Zmrbk <zamirbek.alimbekov3@gmail.com>
2020-07-11 12:30:41 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        // items: 4,
+        // margin: 20,
+        // autoplay: false,
+        // loop: true
+        // nav: true
+    });
+    // $('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
+  });
